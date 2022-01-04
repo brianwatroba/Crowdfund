787b2e41e07231c40e2873f9f413a8c2de749cc2
brian watroba <brianwatroba@gmail.com>
2022-01-04 15:22:15 -0500

    base functions added, comments for todos

--- contracts/project.sol
+++ contracts/project.sol
@@ -3,15 +3,66 @@ pragma solidity ^0.8.11;
 
 contract Project {
   // look into storing them in order to minimize slot use
+  mapping(address => uint256) public contributors;
   address public creator;
   uint256 public fundingGoal;
-  uint256 public endTime;
+  uint256 public minContribution = 0.01 ether;
+  uint256 public totalContributions = 0;
+  uint256 public deadline;
   bool public cancelled = false;
+  // add NFT mappings?
+
+  modifier onlyCreator {
+    require(msg.sender == creator);
+    _;
+  }
+
+  modifier onlyIfActive {
+    require(!cancelled && block.timestamp < deadline && totalContributions < fundingGoal);
+    _;
+    //timestamp issue, front running
+  }
 
   constructor(uint256 _fundingGoal) {
     creator = msg.sender;
     fundingGoal = _fundingGoal;
-    endTime = block.timestamp + 30 days;
+    deadline = block.timestamp + 30 days;
   }
 
+  function contribute(uint256 _amount) external payable onlyIfActive {
+    require(msg.value >= minContribution);
+    uint256 totalContributed = contributors[msg.sender] + _amount; // need safemath
+    contributors[msg.sender] = totalContributed;
+    // check if total contributed matches correct number of NFTs, if not, give NFTs
+  }
+
+  function withdraw(uint256 _amount) external onlyCreator {
+    require (totalContributions >= fundingGoal);
+    require(_amount <= address(this).balance); // is this check built into transfer?
+    // transfer amount to the creator address(msg.sender).transfer(_amount);
+  }
+
+  function getRefund() external {
+    require((totalContributions < fundingGoal && block.timestamp >= deadline) || cancelled);
+    require(contributors[msg.sender] > 0);
+    uint256 contribution = contributors[msg.sender];
+    contributors[msg.sender] -= contribution;
+    // send to them
+  }
+
+  function cancel() external onlyCreator onlyIfActive {
+    cancelled = true;
+  }
+
+
+
+// pure internal function for seeing if time up? guard against front running
+
+
+  // need a fallback function
+  // fallback() external payable
+
 }
+
+//be weary of very big numbers, need a way to catch them
+// which uint should we use?
